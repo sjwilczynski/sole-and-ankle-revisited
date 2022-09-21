c842da86c70b5a346218275301e2a7de643b958b
Stanislaw Wilczynski <stwilczy@microsoft.com>
2022-09-21 17:07:31 +0200

    implement modal menu

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,13 +1,14 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components/macro';
-import { DialogOverlay, DialogContent } from '@reach/dialog';
+import React from "react";
+import styled from "styled-components/macro";
+import {
+  DialogOverlay as DialogOverlayBase,
+  DialogContent as DialogContentBase,
+} from "@reach/dialog";
 
-import { QUERIES } from '../../constants';
-
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import UnstyledButton from "../UnstyledButton";
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
+import { COLORS, WEIGHTS } from "../../constants";
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
   if (!isOpen) {
@@ -15,23 +16,87 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
   }
 
   return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
+    <DialogOverlay>
+      <DialogContent>
+        <CloseButton onClick={onDismiss}>
+          <Icon id="close" strokeWidth={1} />
+          <VisuallyHidden>Close dialog</VisuallyHidden>
+        </CloseButton>
+        <Side />
+        <Nav>
+          <NavLink href="/sale">Sale</NavLink>
+          <NavLink href="/new">New&nbsp;Releases</NavLink>
+          <NavLink href="/men">Men</NavLink>
+          <NavLink href="/women">Women</NavLink>
+          <NavLink href="/kids">Kids</NavLink>
+          <NavLink href="/collections">Collections</NavLink>
+        </Nav>
+        <Side>
+          <Footer>
+            <SecondaryLink href="/terms">Terms and Conditions</SecondaryLink>
+            <SecondaryLink href="/privacy">Privacy Policy</SecondaryLink>
+            <SecondaryLink href="/contact">Contact Us</SecondaryLink>
+          </Footer>
+        </Side>
+      </DialogContent>
+    </DialogOverlay>
   );
 };
+
+const DialogOverlay = styled(DialogOverlayBase)`
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  position: fixed;
+  background-color: hsl(0deg 0% 0% / 0.5);
+  display: flex;
+  justify-content: end;
+`;
+const DialogContent = styled(DialogContentBase)`
+  position: relative;
+  background-color: white;
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 32px;
+`;
+
+const CloseButton = styled(UnstyledButton)`
+  position: absolute;
+  top: 16px;
+  right: 16px;
+`;
+
+const Side = styled.div`
+  flex: 1;
+  display: flex;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+`;
+const NavLink = styled.a`
+  font-size: ${18 / 16}rem;
+  font-weight: ${WEIGHTS.medium};
+  color: ${COLORS["gray"][900]};
+  text-decoration: none;
+  text-transform: uppercase;
+`;
+const SecondaryLink = styled.a`
+  font-size: ${14 / 16}rem;
+  color: ${COLORS["gray"][700]};
+  text-decoration: none;
+`;
+const Footer = styled.footer`
+  align-self: end;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+`;
 
 export default MobileMenu;
